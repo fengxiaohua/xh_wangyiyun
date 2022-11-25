16047a6dd254cb408ca0f339960c4379298086c6
fengxiaohua <472843286@qq.com>
2022-11-25 13:14:10 +0800

    更改线上后段地址

--- src/utils/http/request/config.ts
+++ src/utils/http/request/config.ts
@@ -2,7 +2,7 @@
  * @Author: fengxiaohua 472843286@qq.com
  * @Date: 2022-11-22 15:08:43
  * @LastEditors: fengxiaohua 472843286@qq.com
- * @LastEditTime: 2022-11-22 15:22:06
+ * @LastEditTime: 2022-11-25 11:38:04
  * @FilePath: /vite-vue3-ts/src/utils/http/request/config.ts
  * @Description:
  */
@@ -12,15 +12,16 @@
 // 生成环境: production
 // 测试环境: test
 
-let BASE_URL = ""
+const PROJECT_NAME = "xh_wangyiyun"
+let BASE_URL = `https://www.fastmock.site/mock/eeba86a8af715531dfc7e4be3d6aee25/${PROJECT_NAME}`
 const TIME_OUT = 10000
 
-if (process.env.NODE_ENV === "development") {
-  BASE_URL = "http://127.0.0.1:5173/"
-} else if (process.env.NODE_ENV === "production") {
-  BASE_URL = ""
-} else {
-  BASE_URL = ""
-}
+// if (process.env.NODE_ENV === "development") {
+//   BASE_URL = "http://127.0.0.1:5173/"
+// } else if (process.env.NODE_ENV === "production") {
+//   BASE_URL = ""
+// } else {
+//   BASE_URL = ""
+// }
 
 export { BASE_URL, TIME_OUT }
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
  * @Author: fengxiaohua 472843286@qq.com
  * @Date: 2022-11-02 17:50:34
  * @LastEditors: fengxiaohua 472843286@qq.com
- * @LastEditTime: 2022-11-24 16:55:33
+ * @LastEditTime: 2022-11-24 17:30:58
  * @FilePath: /vite-vue3-ts/vite.config.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -25,6 +25,7 @@ const modifyVars = getThemeVariables({ dark: false })
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [
     vue(),
     WindiCss(),
